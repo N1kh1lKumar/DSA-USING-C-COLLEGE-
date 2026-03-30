50d66f1b905f2d97a890bef6f677d66386d12d45
Nikhil <nikhilkumaraf309@gmail.com>
2026-03-30 11:53:55 +0530

    Find the maximum subarray in a given array (Moore's algo)

---
+++ Array/maxSubArray.cpp
@@ -0,0 +1,26 @@
+//problrm description : Given an integer array nums, find the subarray with the largest sum, and return its sum  
+// leetcode link : https://leetcode.com/problems/maximum-subarray/
+//using Moore's algorithm
+#include<iostream>
+#include<vector>
+#include <climits>
+using namespace std;
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxsum = INT_MIN, curr = 0;
+        for(int val : nums){
+            curr += val;
+            maxsum = max(maxsum, curr);
+            if(curr <0) curr = 0;
+        }
+        return maxsum;}
+};
+
+int main(){
+    Solution s1;
+    vector<int> nums = {-2,1,-3,4,-1,2,1,-5,4};
+    int sum =  s1.maxSubArray(nums);
+    cout << sum << endl;
+    return 0;
+}
