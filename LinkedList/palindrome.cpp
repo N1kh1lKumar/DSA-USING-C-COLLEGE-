d19183d9cadfc4edc5866f2eecc2bace9d871273
Nikhil <nikhilkumaraf309@gmail.com>
2026-04-04 09:52:38 +0530

    Check whether the given linked list is palindrome or not

---
+++ LinkedList/palindrome.cpp
@@ -0,0 +1,69 @@
+//Problem Description : Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+//leetcode link: https://leetcode.com/problems/palindrome-linked-list/    (problem no. 234)
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+
+
+class Solution {
+public:
+    ListNode* reverse(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
+
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        ListNode* front = NULL;
+
+        while (temp) {
+            front = temp->next;
+            temp->next = prev;
+            prev = temp;
+            temp = front;
+        }
+
+        return prev;
+    }
+
+    bool isPalindrome(ListNode* head) {
+
+        if (head == NULL || head->next == NULL)
+            return true;
+         
+        ListNode * slow = head;
+        ListNode* fast = head;
+
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode* newhead = reverse(slow->next);
+        ListNode* first = head;
+        ListNode* second = newhead;
+
+        while (second) {
+            if (first->val != second->val) {
+                reverse(newhead);
+                return false;
+            }
+
+            first = first->next;
+            second = second->next;
+        }
+
+        reverse(newhead);
+
+        return true;
+    }
+};
